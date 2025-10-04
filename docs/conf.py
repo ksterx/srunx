4a9ea221901e189c886362b4da7dcd07648a44ad
ksterx <kostonerx@gmail.com>
2025-10-04 19:12:42 +0900

    add docs

--- docs/conf.py
+++ docs/conf.py
@@ -56,22 +56,5 @@ napoleon_use_rtype = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
-
-# -- Theme options -----------------------------------------------------------
-html_theme_options = {
-    "canonical_url": "",
-    "analytics_id": "",
-    "logo_only": False,
-    "display_version": True,
-    "prev_next_buttons_location": "bottom",
-    "style_external_links": False,
-    "vcs_pageview_mode": "",
-    "style_nav_header_background": "white",
-    "collapse_navigation": True,
-    "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
-}
